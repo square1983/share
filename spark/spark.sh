dab99bd4e997308b07bd20dcdb8ab420b3dd41c1
楊健 <youken1983@gmail.com>
2026-06-03 10:38:09 +0900

    Update spark.sh

--- spark/spark.sh
+++ spark/spark.sh
@@ -1,4 +1,4 @@
 uv run spark-submit \
   --packages \
-org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.6.1,software.amazon.s3tables:s3-tables-catalog-for-iceberg-runtime:0.1.4 \
+org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.6.1,software.amazon.s3tables:s3-tables-catalog-for-iceberg-runtime:0.1.8,software.amazon.awssdk:bundle:2.29.52,software.amazon.awssdk:url-connection-client:2.29.52 \
   write_sales_records_2gb.py
